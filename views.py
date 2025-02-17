8b53eafd523d2bb46eac6140a7cf4edf1a3c4e6f
Sangam Bhamare <84763687+sangambhamare@users.noreply.github.com>
2025-02-17 16:03:22 +0000

    Update views.py

--- views.py
+++ views.py
@@ -4,11 +4,41 @@
 # views.py
 import os
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import chess
 
 def index(request):
-    return render(request, "index.html")
+    return HttpResponse('''<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Chess Game</title>
+</head>
+<body>
+    <h1>Play Chess</h1>
+    <button onclick="startGame()">New Game</button>
+    <input type="text" id="move" placeholder="Enter move e.g., e2e4">
+    <button onclick="makeMove()">Make Move</button>
+    <p id="game-status"></p>
+    <script>
+        function startGame() {
+            fetch("/new-game/")
+                .then(response => response.json())
+                .then(data => document.getElementById("game-status").innerText = "New Game Started: " + data.fen);
+        }
+
+        function makeMove() {
+            let move = document.getElementById("move").value;
+            fetch("/make-move/", {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: "move=" + move
+            })
+            .then(response => response.json())
+            .then(data => document.getElementById("game-status").innerText = data.status ? data.fen : data.error);
+        }
+    </script>
+</body>
+</html>")
 
 def new_game(request):
     board = chess.Board()
